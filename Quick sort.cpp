a9e987015cc6b71b8274529767ef5687939cc46d
Mitansh007 <100666301+Mitansh007@users.noreply.github.com>
2022-05-05 08:37:15 +0530

    Update Quick sort.cpp

--- Quick sort.cpp
+++ Quick sort.cpp
@@ -1,52 +1,89 @@
-//Quick sort - recursive
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-  
-void swap(int* a, int* b)
+
+void swap(int *x, int *y)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+    int temp=*x;
+    *x=*y;
+    *y=temp;
 }
 
 int partition(int arr[], int l, int h)
 {
-    int x = arr[h];
-    int i = (l - 1);
- 
-    for (int j = l; j <= h - 1; j++) {
-        if (arr[j] <= x) {
+    int pivot = arr[l];
+    int i=l;
+    int j=h;
+    do
+    {
+        do
+        {
             i++;
+        }while(arr[i]<=pivot);
+        do
+        {
+            j--;
+        }while(arr[j]>pivot);
+        if(i<j)
+        {
             swap(&arr[i], &arr[j]);
         }
-    }
-    swap(&arr[i + 1], &arr[h]);
-    return (i + 1);
+    }while(i<j);
+    
+    swap(&arr[l], &arr[j]);
+    return j;
 }
 
-void quickSort(int A[], int l, int h)
+void Rquicksort(int arr[], int l, int h)
 {
-    if (l < h) {
-        int p = partition(A, l, h);
-        quickSort(A, l, p - 1);
-        quickSort(A, p + 1, h);
+    int j;
+    if(l<h)
+    {
+        j=partition(arr, l, h);
+        Rquicksort(arr, l, j);
+        Rquicksort(arr, j+1, h);
     }
 }
- 
+
+void Iquicksort(int arr[], int l, int h)
+{
+    int stack[h-l+1];
+    int top=-1;
+    stack[++top]=l;
+    stack[++top]=h;
+    while(top>=0)
+    {
+        h=stack[top--];
+        l=stack[top--];
+        
+        int p=partition(arr,l,h);
+        
+        if(p-1>l)
+        {
+            stack[++top]=l;
+            stack[++top]=p-1;
+        }
+        if(p+1<h)
+        {
+            stack[++top]=p+1;
+            stack[++top]=h;
+        }
+    }
+}
+
 int main()
 {
-    int size;
-	cout<<"Enter size"<<endl;
-	cin>>size;
-	int A[size];
-	cout<<"Enter array elements"<<endl;
-	for(int i=0;i<size;i++)
-	{
-		cin>>A[i];
-	}
-    quickSort(A, 0, size - 1);
-    for (int i = 0; i < size; i++) {
-        cout << A[i] << " ";
+    int n;
+    cout<<"Enter the length of array"<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"Enter all the elements of array"<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
     }
-    return 0;
+    Iquicksort(arr, 0, n);
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
 }

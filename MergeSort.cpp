a1f98f44c9fb33d47083d03a30a1b9fd6652892e
Mitansh007 <100666301+Mitansh007@users.noreply.github.com>
2022-05-05 08:35:43 +0530

    Update MergeSort.cpp

--- MergeSort.cpp
+++ MergeSort.cpp
@@ -1,64 +1,71 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-void Display(int Arr[],int n)
-{
-	for(int i=0;i<n;i++)
-	{
-		cout<<Arr[i]<<" ";
-	}
-	cout<<"\n";	
-}
 
-void Merge(int A[], int l, int mid, int h)
+void merge(int A[], int l, int mid, int h)
 {
-	int B[100];
-	int i=l,j=mid+1,k=l;
-	while(i<=mid && j<=h)
-	{
-		if(A[i]<A[j])
-		{
-			B[k++]=A[i++];
-		}
-		else
-		{
-			B[k++]=A[j++];
-		}
-	}
-	for(;i<=mid;i++)
+    int i=l, j=mid+1, k=l;
+    int B[100];
+    while(i<=mid && j<=h)
+    {
+        if(A[i]<A[j])
+            B[k++]=A[i++];
+        else
+            B[k++]=A[j++];
+    }
+    for(;i<=mid;i++)
         B[k++]=A[i];
-	for(;j<=h;j++)
+    for(;j<=h;j++)
         B[k++]=A[j];
-	 for(i=l;i<=h;i++)
+
+    for(i=l;i<=h;i++)
         A[i]=B[i];
 }
 
-void MergeSort(int arr[], int l, int h)
+void Rmergesort(int arr[], int l, int h)
 {
-    int m;
-    if (l < h)
+    int mid;
+    if(l<h)
     {
-        m = (l + h) / 2;
-        MergeSort(arr, l, m);
-        MergeSort(arr, m + 1, h);
-        Merge(arr, l, m, h);
+        mid=(l+h)/2;
+        Rmergesort(arr,l,mid);
+        Rmergesort(arr,mid+1,h);
+        merge(arr,l,mid,h);
     }
 }
 
+void Imergesort(int arr[], int n)
+{
+    int p, l, mid, h, i;
+    for(p=2;p<=n;p=p*2)
+    {
+        for(i=0;i+p-1<n;i=i+p)
+        {
+            l=i;
+            h=i+p-1;
+            mid=(l+h)/2;
+            merge(arr,l,mid,h);
+        }
+    }
+    if(p/2<n)
+    {
+        merge(arr,0,p/2-1,n-1);
+    }
+}
 
-int main(){
-	int size;
-	cout<<"Enter size"<<endl;
-	cin>>size;
-	int A[size];
-	cout<<"Enter array elements"<<endl;
-	for(int i=0;i<size;i++)
-	{
-		cin>>A[i];
-	}
-
-	MergeSort(A,0,size-1);
-    cout<<"After Merging"<<endl;
-	Display(A,size);
-	
-	return 0;
+int main()
+{
+    int n;
+    cout<<"Enter the length of array"<<endl;
+    cin>>n;
+    int arr[n];
+    cout<<"Enter all the elements of array"<<endl;
+    for(int i=0; i<n; i++)
+    {
+        cin>>arr[i];
+    }
+    Imergesort(arr, n);
+    for(int i=0; i<n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
 }
